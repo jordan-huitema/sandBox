d8b265318b12ababdb3e7c2096555d5c39e91775
Jordan <jordan-huitema@live.nmit.ac.nz>
2020-03-03 11:39:38 +1300

    commit

--- sandboxWeek2.js
+++ sandboxWeek2.js
@@ -7,3 +7,23 @@ firstName = "Fredrick";
 
 console.log(firstName);
 */
+
+// Variable declared in global scope
+var globalVariable = 'I am global';
+const globalConstVariable = 'I am also global';// Try to access the value of globalVariable
+globalVariable;
+// 'I am global'globalConstVariable
+// 'I am also global'
+// Variable declared in local scope (function scope) example 1:
+function myFunc() {
+  var myLocalVariable = 'I am local';
+  let myLocalLetVariable = 'I am also local';  // Try to access the value of myLocalVariable
+  // from the inside of the myFunc function
+  myLocalVariable;
+  // 'I am local'  myLocalLetVariable
+  // 'I am also local'
+};// Try to access the value of myLocalVariable
+// from the outside of the myFunc function
+myLocalVariable;
+// ReferenceError: myLocalVariable is not definedmyLocalLetVariable
+// ReferenceError: myLocalVariable is not defined
